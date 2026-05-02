0bbd075434dc707f24279356c158571196842145
AngelAmoSanchez <angelamosnchz@gmail.com>
2026-05-02 20:44:36 +0200

    feat: bajada de poll de la aplicación

--- backend-cloud/src/database/connection.py
+++ backend-cloud/src/database/connection.py
@@ -29,8 +29,8 @@ class Database:
             # Configuración del engine
             engine_kwargs = {
                 "echo": settings.debug,
-                "pool_size": 5,
-                "max_overflow": 10,
+                "pool_size": 2,
+                "max_overflow": 3,
                 "pool_recycle": 300,
                 "pool_pre_ping": True,
                 "pool_timeout": 10,
